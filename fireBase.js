60007c7ddec9c2c4694cb884082a3d385ff5231f
Bikds <30985142+Bikds@users.noreply.github.com>
2019-07-22 11:00:43 -0700

    Update fireBase.js

--- fireBase.js
+++ fireBase.js
@@ -12,20 +12,15 @@ var app_fireBase = {};
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
   app_fireBase = firebase;
 })()
 
 function save_data() {
   if (typeof (Storage) !== "undefined") {
-
     // Store
-    localStorage.setItem("email", document.getElementById('user_email').value);
-    localStorage.setItem("first", document.getElementById('user_first').value);
-    localStorage.setItem("last", document.getElementById('user_last').value);
+    localStorage.setItem("nickName", document.getElementById('user_nickname').value);
     localStorage.setItem("age", document.getElementById('user_age').value);
     localStorage.setItem("gender", document.getElementById('user_gender').value);
-    localStorage.setItem("country", document.getElementById('user_country').value);
     // window.location.href = "index.html";
     // Retrieve
     // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
@@ -39,13 +34,11 @@ function save_data() {
 
 function submitform() {
   var f = document.getElementsByTagName('form')[0];
-  console.log("hi");
   if (f.checkValidity()) {
     console.log("test1");
     window.location.href = "quiz.html";
     f.submit();
     save_data();
-    console.log("Test");
     window.location.href = "quiz.html";
   } else {
     alert("Please fill out all the fields. Remember your email field must include an '@' symbol");
@@ -53,42 +46,41 @@ function submitform() {
   }
 }
 
-
-function print_data(){
+function print_data() {
   var databaseRef = firebase.database().ref('users/');
   var container = document.getElementById("user_list");
 
-  databaseRef.once('value', function(snapshot) {
-     snapshot.forEach(function(childSnapshot) {
-     var childKey = childSnapshot.key;
-     var childData = childSnapshot.val();
+  databaseRef.once('value', function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      var childKey = childSnapshot.key;
+      var childData = childSnapshot.val();
 
-    var user_card = document.createElement("div");
-    container.appendChild(user_card);
-    user_card.className = "user_card";
-    user_card.addEventListener("click", function(){ sendToArduino(childData.user_scentArray); });
+      var user_card = document.createElement("div");
+      container.appendChild(user_card);
+      user_card.className = "user_card";
+      user_card.addEventListener("click", function () { sendToArduino(childData.user_scentArray); });
 
-    var firstNode = document.createElement("h1");
-    var holderName = childData.user_first;
-    if (holderName == undefined){
-      holderName = "John";
-    }
-    firstNode.innerHTML = holderName;
-    user_card.appendChild(firstNode);
+      var firstNode = document.createElement("h1");
+      var holderName = childData.user_first;
+      if (holderName == undefined) {
+        holderName = "John";
+      }
+      firstNode.innerHTML = holderName;
+      user_card.appendChild(firstNode);
 
-    var lastNode = document.createElement("h2");
-    holderName = childData.user_last;
-    if (holderName == undefined){
-      holderName = "Doe";
-    }
-    lastNode.innerHTML = holderName;
-    user_card.appendChild(lastNode);
+      var lastNode = document.createElement("h2");
+      holderName = childData.user_last;
+      if (holderName == undefined) {
+        holderName = "Doe";
+      }
+      lastNode.innerHTML = holderName;
+      user_card.appendChild(lastNode);
     });
   });
 }
 
 
-function sendToArduino(scent){
+function sendToArduino(scent) {
   alert("Floral: " + scent[0] +
     "\nWarm/Spicy: " + scent[1] +
     "\nWoodsy: " + scent[2] +
